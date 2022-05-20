c3e7b9474b43db424c75fe326eb5eb4a0a6a89d3
oddCompost <richomas@hotmail.com>
2022-05-20 14:01:42 +0100

    fix for alfie

--- manageUser.js
+++ manageUser.js
@@ -1,6 +1,6 @@
 //Login
 //Register
-const { exists } = require("dottie");
+
 const sqlite3 = require("sqlite3");
 
 //connect to DB
@@ -18,8 +18,75 @@ const db = new sqlite3.Database(
 
 let loginInput = {loginUsername:"testUsername", loginPassword:"testPassword"}
 
-let existsUser = db.run(`SELECT EXISTS (SELECT 1 FROM users WHERE username = 'testUsername')`);
+//let existsUser = db.run(`SELECT * FROM users`);
 
-console.log(existsUser);
+getData();
+getSelectedUserData();
+
+//Handle get all data back from database
+async function getData(){
+
+	let myVar = await handleGetAllData();
+
+	console.log(myVar);
+
+}
+
+//Handle get selected data back from database
+async function getSelectedUserData(){
+
+	let myVar = await handleGetSelectedUserData();
+
+	console.log(myVar);
+
+}
+
+
+//Handle get selected data back from database
+function handleGetSelectedUserData(){
+      const  selectSql = `SELECT * FROM users WHERE username = 'testUsername'`;
+
+      return new Promise( (resolve, reject) => {    
+
+                  db.all(selectSql, (err, rows) => {
+            
+                         if(err){
+                  
+                              reject(new Error(err.message));
+                  
+                         } else {
+      
+                              resolve(rows);
+                  
+                         } 
+      
+                   });
+
+            });
+      };
+
+
+//Hanlde get all data back from database
+function handleGetAllData(){
+      const  selectSql = `SELECT * FROM users`;
+
+      return new Promise( (resolve, reject) => {    
+
+                  db.all(selectSql, (err, rows) => {
+            
+                         if(err){
+                  
+                              reject(new Error(err.message));
+                  
+                         } else {
+      
+                              resolve(rows);
+                  
+                         } 
+      
+                   });
+
+            });
+      };
 
 //db.run("INSERT INTO users(username, password) VALUES('"+loginInput.loginUsername+"', '"+loginInput.loginPassword+"')");
