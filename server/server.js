27f6ed883c3701515a4c23ef09a91241ad0236e7
Richard <richomas@hotmail.com>
2022-05-22 21:48:04 +0100

    socket io working to server

--- server/server.js
+++ server/server.js
@@ -1,61 +1,71 @@
 const express = require('express');
-const sqlite3 = require("sqlite3");
+const sqlite3 = require('sqlite3');
 
-const io = require("socket.io")(4000, {
-      cors:{origin:  '*'},
-});
-
-const dbMethods = require("./dbConnection");
+const dbMethods = require('./dbConnection');
 const app = express();
 const port = 3000;
 
-//Socket.io testing stuff
+const socket = require('socket.io')(8080, {
 
-io.on('connection', (socket) => {
-
-      console.log(socket.id);
+	cors: { origin: '*' }
 
 });
+
+//Socket.io testing stuff
+
+socket.on('connection', (socket) => {
+      
+	console.log(socket.id);
+
+      socket.on('message', (message) => {
+
+            console.log(message);
+      
+      });
+      
+});
+
+
+
+
+
 
 
 //----------------
 
-app.use(express.static('public'));
+app.use(express.static('../public'));
 
 app.listen(port, () => {
-    console.log(`Server listening at http://localhost:${port}`)
+	console.log(`Server listening at http://localhost:${port}`);
 });
 
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //*--------------------DB Connection-----------------------
 
-
 dbMethods.connectToDb();
 
-
 //*-------------------------API---------------------------
-
-
 
 //*------- Get Requests ---------
 
 // * Promise based
-app.get("/getusers/:id", async (request, response) => {
+app.get('/getusers/:id', async (request, response) => {
+	const test = await dbMethods.getDataById(request.params.id); //Parses in a obj
 
-      const test = await dbMethods.getDataById(request.params.id); //Parses in a obj
-      
-      response.json(cafes);
-
+	response.json(cafes);
 });
 
 // * Promise based
-app.get("/getAllUsers", async (request, response) => {
+app.get('/getAllUsers', async (request, response) => {
+	const test = await dbMethods.getAllData(); //Parses in a obj
 
-      const test = await dbMethods.getAllData(); //Parses in a obj
-      
-      response.json(test);
-
+	response.json(test);
 });
 
+// sendFile will go here
+app.get('/home', function (req, res) {
+	console.log('hit');
+	res.sendFile('../public/index.html');
+});
