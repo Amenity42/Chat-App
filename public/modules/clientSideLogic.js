c63c11843cb8e5666560a7bd9d0d21ee194aded9
oddCompost <richomas@hotmail.com>
2022-05-25 13:45:14 +0100

    adding all to main

--- public/modules/clientSideLogic.js
+++ public/modules/clientSideLogic.js
@@ -1,7 +1,12 @@
 
-
 const socket = io('http://localhost:8080');
+const messageInput = document.getElementById('messageInput');
+const messageContainer = document.getElementById('messageContainer');
+const sendBtn = document.getElementById('messageSend');
+
 let username = undefined;
+let animCounter = 0;
+let notificationsArray = [];
 
 class Message {
 	constructor(message, user) {
@@ -11,23 +16,25 @@ class Message {
 }
 
 
-
+//Set user name at top of screen
 function setUserName(name) {
+
+	const userNameDisplay = document.getElementById('userNameDisplay')
 
 	username = name;
 
-	console.log(`Username: ${username}`);
+	// console.log(`Username: ${username}`);
+
+	userNameDisplay.innerHTML = username;
 
 }
 
+//Set the user name then let the server know who logged in so it can be broadcast
 setUserName(localStorage.getItem('userName'));
+onlineNotification();
 
-console.log(`Username: ${username}`);
 
 
-const messageInput = document.getElementById('messageInput');
-const messageContainer = document.getElementById('messageContainer');
-const sendBtn = document.getElementById('messageSend');
 
 if(sendBtn !== null || messageInput !== null){
 
@@ -76,16 +83,94 @@ function postMessageToChat(message) {
 	console.table(message);
 }
 
+function notifications(notification){
+
+	const notificationElement = document.getElementById("notification");
+	const notificationText = document.getElementById('notificationText');
+
+	if(notification !== undefined){
+		notificationsArray.push(notification);
+	}
+	
+	console.log(notificationsArray);
+
+	if(animCounter === 0){
+		animCounter = 1;
+		//Display notification
+
+		notificationElement.style.display = 'block';
+
+		notificationText.innerHTML = `${notificationsArray[notificationsArray.length -1]} Just logged in!`;
+
+		//Animation timers 
+		setTimeout(() => {
+
+			notificationElement.style.opacity = 1;
+			notificationText.style.opacity = 1;
+			notificationElement.style.width = '300px';
+			
+		}, 100);
+
+		setTimeout(() => {
+
+			notificationElement.style.opacity = 0;
+			notificationElement.style.width = '0px'; 
+			
+		}, 2000);
+		
+		setTimeout(() => {
+
+			notificationElement.style.display = 'none';
+
+			notificationsArray.pop();
+
+			console.log(notificationsArray);
+			animCounter = 0;
+
+			if(notificationsArray.length > 0){
+				notifications();
+			}
+
+		}, 3000);
+
+
+	}
+
+
+
+	}
+
+
+
+
+//*------------------------Sockets------------------------
 function sendDataToServer(message){
 
 	socket.emit('message', message);
 
 }
 
+function onlineNotification(){
+
+	socket.emit('userLoggedIn', username);
+
+}
+
+
 socket.on('recMessage', (message) => {
 
 	postMessageToChat(message);
 
 } );
+
+//Display notifications to screen
+socket.on('notification', (notification) => {
+
+	notifications(notification);
+
+} );
+
+
+
 
 
